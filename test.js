8d1f12bc5ec7e7120c285723e32ce81dcb6360cf
Andrew Berry <andy+github@berry.email>
2017-10-18 11:10:58 -0400

    Replace expression with placeholder

--- test.js
+++ test.js
@@ -17,21 +17,7 @@ describe('styled-jsx-plugin-sass', () => {
     )
   })
 
-  it('works with "styled-jsx-expression" placeholders', () => {
-    assert.equal(
-      plugin('p { img { display: block } color: %%styled-jsx-expression-0%%; } %%styled-jsx-expression-1%%').trim(),
-      cleanup(`
-        p {
-          color: %%styled-jsx-expression-0%%; }
-          p img {
-            display: block; }
-
-        %%styled-jsx-expression-1%%
-      `)
-    )
-  })
-
-  it('works with "styled-jsx-placeholder" placeholders', () => {
+  it('works with placeholders', () => {
     assert.equal(
       plugin('p { img { display: block } color: %%styled-jsx-placeholder-0%%; } %%styled-jsx-placeholder-1%%').trim(),
       cleanup(`
