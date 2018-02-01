615ac472a3caf66f50239ab6c1c053a1fb3eb911
Daniel Gamage <hello@danielgamage.com>
2018-02-01 03:59:23 -0500

    match comma-separated placeholders (#14)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sass = require('node-sass')
 
 module.exports = (css, settings) => {
   const cssWithPlaceholders = css
-    .replace(/%%styled-jsx-placeholder-(\d+)%%(\w*\s*[);{])/g, (_, id, p1) =>
+    .replace(/%%styled-jsx-placeholder-(\d+)%%(\w*\s*[),;{])/g, (_, id, p1) =>
       `styled-jsx-placeholder-${id}-${p1}`
     )
     .replace(/%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
@@ -14,7 +14,7 @@ module.exports = (css, settings) => {
   }, settings.sassOptions)).css.toString()
 
   return preprocessed
-    .replace(/styled-jsx-placeholder-(\d+)-(\w*\s*[);{])/g, (_, id, p1) =>
+    .replace(/styled-jsx-placeholder-(\d+)-(\w*\s*[),;{])/g, (_, id, p1) =>
       `%%styled-jsx-placeholder-${id}%%${p1}`
     )
     .replace(/\/\*%%styled-jsx-placeholder-(\d+)%%\*\//g, (_, id) =>
